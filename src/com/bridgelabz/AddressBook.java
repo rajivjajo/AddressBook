b5c637bdc1abda348f4e2e0a2daa3e6e3bf9b645
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-17 03:51:22 +0530

    Creating and adding contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,2 +1,20 @@
-package com.bridgelabz;public class AddressBook {
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    public ArrayList<Contact> contacts;
+
+    public AddressBook() {
+        contacts = new ArrayList<Contact>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
 }
+
